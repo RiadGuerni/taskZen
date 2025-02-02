f047afb9041ada78abd132794d546d598b130143
RiadGuerni <riadguerni77@gmail.com>
2025-02-02 02:11:51 -1100

    Configure Passport.js with JWT strategy for authentication
    Integrated Passport.js with the JWT strategy to handle user authentication.
    Configured Passport to extract and verify JWTs from incoming requests, enhancing security for protected routes.

---
+++ backend/config/passport_jwt.js
@@ -0,0 +1,21 @@
+const passport = require("passport");
+const JwtStrategy = require("passport-jwt").Strategy;
+const User = require("../models/user");
+const options =  {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: process.env.JWT_SECRET, 
+  }
+passport.use(new JwtStrategy(options,async  (jwt_payload, done) => {
+    try{
+    const userId = jwt_payload.userId;
+    const user =  await User.findById(userId);
+    if (user) {
+        return done(null, user);
+    }
+    return done(null, false);
+    }
+    catch (err) {
+        return done(err, false);
+    }
+   
+}))
